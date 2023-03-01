d41c32d1dd73e1b2f95201ff16bbda28df63e1ab
Sldvlld <sladvlad@gmail.com>
2023-03-01 17:31:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
 
+const input = document.querySelector('#search-box');
+console.log(input);
+input.addEventListener('input', onInput)
+function onInput(ev) {
+    
+}
+console.log(onInput);
+
 const DEBOUNCE_DELAY = 300;
