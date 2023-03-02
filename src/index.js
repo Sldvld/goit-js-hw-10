128e100df8a88ec4821b8b7f0a37951f6613b2f1
Sldvlld <sladvlad@gmail.com>
2023-03-02 19:27:14 +0200

    add listMarkup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
+import notiflix from 'notiflix'
 import './css/styles.css';
 
-const input = document.querySelector('#search-box');
-console.log(input);
-input.addEventListener('input', onInput)
-function onInput(ev) {
-    
-}
-console.log(onInput);
-
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('country-info');
+
+input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+
+function onInput(evt) {
+    const countryName = evt.target.value.trim();
+    console.log(countryName);
+    if(countryName === "") {
+        countryList.innerHTML = '';
+        countryName.innerHTML = '';
+    return;
+    }
+    fetchCountries(countryName)
+    .then(renderCountries)
+    .catch(onFetchError);
+}
+function renderCountries(countryName) {
+    const markup = countryName.map(({name, flags}) => {
+        return `<li><img src="${flags.svg}" alt="${flags.alt}"width="30" height="20"><span>${name.official}</span></li>`;
+    }).join("");
+    countryList.innerHTML = markup;
+}
+
+function onFetchError() {
+
+}
