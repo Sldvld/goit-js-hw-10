4b54a1342397f36b2b8b824e722737ef64f74602
Sldvlld <sladvlad@gmail.com>
2023-03-06 22:15:00 +0200

    prettier

--- src/index.js
+++ src/index.js
@@ -11,37 +11,36 @@ const countryInfo = document.querySelector('.country-info');
 input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(evt) {
-    const countryName = evt.target.value.trim();
+  const countryName = evt.target.value.trim();
 
-        if(countryName === '') {
-            clearTpl()
-        return;
-    }
-    fetchCountries(countryName)
+  if (countryName === '') {
+    clearTpl();
+    return;
+  }
+  fetchCountries(countryName)
     .then(countryName => {
-        if (countryName.length > 10) {
-            Notify.info(
-                'Too many matches found. Please enter a more specific name.'
-            );
-        clearTpl()
+      if (countryName.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        clearTpl();
         return;
-    }
-    renderTpl(countryName);
+      }
+      renderTpl(countryName);
     })
-    .catch (error => {
-        clearTpl()
-            Notify.warning(
-                'Oops, there is no country with that name'
-            );
+    .catch(error => {
+      clearTpl();
+      Notify.warning('Oops, there is no country with that name');
     });
-};
+}
 
 function renderTpl(countryName) {
-        clearTpl();
+  clearTpl();
 
-    if (countryName.length === 1) {
-        const markup = countryName.map(({name, flags, capital, population, languages}) => {
-            return `<div class="country-card"><img 
+  if (countryName.length === 1) {
+    const markup = countryName
+      .map(({ name, flags, capital, population, languages }) => {
+        return `<div class="country-card"><img 
             src='${flags.svg}' 
             alt='${name.official}' 
             width="120" 
@@ -65,24 +64,26 @@ function renderTpl(countryName) {
             </ul>
             </div>
             `;
-        }).join();
-        countryInfo.innerHTML = markup;
-    }
-    else{
-        const markup = countryName.map(({name, flags}) => {
-            return `<li class="country-list__item">
+      })
+      .join();
+    countryInfo.innerHTML = markup;
+  } else {
+    const markup = countryName
+      .map(({ name, flags }) => {
+        return `<li class="country-list__item">
             <img src="${flags.svg}" 
             alt="${flags.alt}
             "width="30" 
             height="20">
             <span>${name.official}</span>
             </li>`;
-        }).join("");
-        countryList.innerHTML = markup;
-    }
-};
+      })
+      .join('');
+    countryList.innerHTML = markup;
+  }
+}
 
 function clearTpl() {
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
 }
